1c2db39812ef69542c2c34a078aea1a79892a618
Unknown <mckyna@icloud.com>
2018-01-22 08:43:09 -0700

    Assignment 1

---
+++ assign1.js
@@ -0,0 +1,12 @@
+/*jslint devel: true, node: true*/
+
+
+var firstName, lastName, color;
+firstName = prompt('What is your first name');
+lastName = prompt('last name ?');
+color = prompt('what is your favorite color ?');
+alert('Her first name is' + firstName);
+alert('full name is ' + firstName + ' ' + lastName);
+alert('favorite color is ');
+
+
